b7f85650eb95d4d1e900de0847f425fa2f49a43f
Jamesford <jamie@stobbs.me>
2018-05-12 13:36:03 +0200

    Fix trailing spaces in dictionaries, update tests

--- tests/index.js
+++ tests/index.js
@@ -1,42 +1,60 @@
 var expect = require('chai').expect
 var SillyId = require('../index.js')
 
-describe('Default ID Generation', function() {
+describe('Default ID Generation', () => {
 
   var sillyid = new SillyId()
 
-  it('should generate an ID like HungryOrangeGeckos', function() {
+  it('should generate an ID like HungryOrangeGeckos', () => {
     var result = sillyid.generate()
     expect(result).to.match(/^([A-Z]{1}[a-z]+){3}$/)
   })
 })
 
-describe('Custom Spacer', function() {
+describe('Custom Spacer', () => {
 
   var sillyid = new SillyId(undefined, '-', undefined)
 
-  it('should generate an ID like Hungry-Orange-Geckos', function() {
+  it('should generate an ID like Hungry-Orange-Geckos', () => {
     var result = sillyid.generate()
     expect(result).to.match(/^([A-Z]{1}[a-z]+-){2}([A-Z]{1}[a-z]+){1}$/)
   })
 })
 
-describe('No Capitalisation', function() {
+describe('No Capitalisation', () => {
 
   var sillyid = new SillyId(undefined, undefined, false)
 
-  it('should generate an ID like hungryorangegeckos', function() {
+  it('should generate an ID like hungryorangegeckos', () => {
     var result = sillyid.generate()
     expect(result).to.match(/^([a-z]+){3}$/)
   })
 })
 
-describe('Custom Spacer & No Capitalisation', function() {
+describe('Custom Spacer & No Capitalisation', () => {
 
   var sillyid = new SillyId(undefined, '-', false)
 
-  it('should generate an ID like hungry-orange-geckos', function() {
+  it('should generate an ID like hungry-orange-geckos', () => {
     var result = sillyid.generate()
     expect(result).to.match(/^([a-z]+-){2}([a-z]+){1}$/)
+  })
+})
+
+describe('Custom letter sequence (npm)', () => {
+
+  var sillyid = new SillyId([
+    { type: 'adj', letter: 'n' },
+    { type: 'adj', letter: 'p' },
+    { type: 'noun', letter: 'm' },
+  ], '-', undefined)
+
+  it('should generate an ID like Naughty-Powderblue-Moray', () => {
+    var result = sillyid.generate()
+    var parts = result.split('-')
+
+    expect(parts[0]).to.match(/^N[a-z]+$/)
+    expect(parts[1]).to.match(/^P[a-z]+$/)
+    expect(parts[2]).to.match(/^M[a-z]+$/)
   })
 })
